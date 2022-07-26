be4eccd082d052805c3376de1ceaa7b1c8ead747
king <emmanueltakor4@gmail.com>
2022-07-26 10:10:54 -0400

    commits

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,15 +1,20 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { CreateUserDto } from './dto/create.user.dto';
+import { UsersService } from './users.service';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
   @Get()
   getUsers(): any {
-    return [{ id: 0 }];
+    return this.usersService.findAll();
   }
   @Get(':id')
   getUserById(@Param('id') id: string): any {
-    return {
-      id,
-    };
+    return this.usersService.findById(Number(id));
+  }
+  @Post()
+  createUser(@Body() body: CreateUserDto): any {
+    return this.usersService.createUser(body.name);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create.user.dto';
+import { UsersService } from './../../../users/users.service';
+import { Injectable, Post } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users: any = [{ id: 0, name: 'Marius' }];
+  findAll() {
+    return this.users;
+  }
+  findById(userId: number) {
+    return this.users.find((user) => user.id === userId);
+  }
+  createUser(createUserDto: CreateUserDto) {
+    const newUser = { id: Date.now(), ...createUserDto };
+    this.users.push(newUser);
+    return newUser;
+  }
+}
